14b8eedac63a065335682173f3bc000082866674
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2022-12-07 13:05:00 -0300

    cree los métodos de la clase Equipo

--- tp2/src/Equipo.java
+++ tp2/src/Equipo.java
@@ -3,4 +3,20 @@ import java.util.ArrayList;
 public class Equipo {
     private String nombre;
     private ArrayList<Partido> partidosJugados;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Partido> getPartidosJugados() {
+        return partidosJugados;
+    }
+
+    public void setPartidosJugados(ArrayList<Partido> partidosJugados) {
+        this.partidosJugados = partidosJugados;
+    }
 }
