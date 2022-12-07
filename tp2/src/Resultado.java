6e49551af700c559894a18f8aaf322f55f6efa51
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2022-12-07 13:03:27 -0300

    feat: completé la clase Resultado con sus métodos correspondientes

--- tp2/src/Resultado.java
+++ tp2/src/Resultado.java
@@ -1,4 +1,31 @@
 public class Resultado {
     private int golesLocal;
     private int golesVisitante;
+    private boolean ganoLocal;
+    private boolean empate;
+
+
+    public boolean ganoLocal(){
+        return ganoLocal;
+    }
+
+    public boolean Empate(){
+        return empate;
+    }
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
 }
