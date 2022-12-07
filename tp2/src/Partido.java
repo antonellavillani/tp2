5732a68195c6bfc525ed18909b5b04de05896c55
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2022-12-07 12:47:06 -0300

    feat: cree los getters y setters de los atributos de la clase Partido

--- tp2/src/Partido.java
+++ tp2/src/Partido.java
@@ -6,4 +6,42 @@ public class Partido {
     private Equipo visitante;
     private Resultado resultado;
 
+
+    public Partido (Date fecha, Equipo local, Equipo visitante){
+        this.fecha = fecha;
+        this.local = local;
+        this.visitante = visitante;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Equipo getLocal() {
+        return local;
+    }
+
+    public void setLocal(Equipo local) {
+        this.local = local;
+    }
+
+    public Equipo getVisitante() {
+        return visitante;
+    }
+
+    public void setVisitante(Equipo visitante) {
+        this.visitante = visitante;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Resultado resultado) {
+        this.resultado = resultado;
+    }
 }
